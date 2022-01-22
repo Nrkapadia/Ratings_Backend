72b11c7c488aaf9569f8a86585ffd472aff12f00
Nrkapadia <75656940+Nrkapadia@users.noreply.github.com>
2022-01-22 16:17:43 +0530

    again

--- routes/UserRatingRoute.js
+++ routes/UserRatingRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ratingController = require("./../controll/controller");
 
-router.route("/").get(ratingController.getAll).put(ratingController.createOne);
+router.route("/").get(ratingController.getAll).post(ratingController.createOne);
 
 router.route("/getOne").post(ratingController.getByUid);
 
